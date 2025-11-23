12ca64be383e2ff93188e9c1e771929a838ecbd4
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-11-23 15:21:40 +0800

    幫sqlize加防抖

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,4 +5,11 @@ export const sqlize = new Sequelize(process.env.MYSQL_URL!, {
   dialect: "mysql",
   logging: false,
   dialectOptions: { charset: "utf8mb4" },
+  timezone: "+08:00",
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
